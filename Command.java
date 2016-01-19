61316db9b0ae756f9b76bbd703d51639b1060859
yellmyheart <b02902019@ntu.edu.tw>
2016-01-19 18:53:41 +0800

    first commit

---
+++ Command.java
@@ -0,0 +1,16 @@
+public class Command{
+
+	static final int REGISTER = -10,
+					 LOGIN = -11;	
+
+	static final int KNOCKING = 0,
+					 KNOCKING_ACK = 1,
+					 SEND_MSG = 2,
+					 SEND_MSG_ACK = 3,
+					 SEND_FILE = 4,
+					 SEND_FILE_ACK = 5,
+					 SEND_MSG_CHAT = 6,
+					 SEND_MSG_CHAT_ACK = 7,
+					 SEND_FILE_CHAT = 8,
+					 SEND_FILE_CHAT_ACK = 9; 
+}
