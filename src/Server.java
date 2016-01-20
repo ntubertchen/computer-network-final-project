7be586a972669e34fc38ab81d0a3e132b476eaac
陳 柏均 <bert308052004@Macintosh.local>
2016-01-21 01:24:40 +0800

    a

--- src/Server.java
+++ src/Server.java
@@ -4,16 +4,19 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
 import java.lang.ClassNotFoundException;
+import java.util.HashMap;
+import java.util.ArrayList;
 class Server extends Thread{
-	public void run(){
+	HashMap<Integer,ArrayList<Header> > workQueue;
+	HashMap<Integer,ArrayList<Header> >	dataQueue;
 
-	}
 	public static void main(String[] args) {
+		this.workQueue = new HashMap<Integer,ArrayList<Header> >();
+		this.dataQueue = new HashMap<Integer,ArrayList<Header> >();
 		ServerSocket socket;
 		try{
 			socket = new ServerSocket(8000);
 			while(true){
-				System.out.println("try catch");
 				try{
 					Socket temp_socket = socket.accept();
 					try{
@@ -34,6 +37,41 @@ class Server extends Thread{
 			}
 		}catch(IOException eee){
 			System.out.println("server failure");
+		}
+	}
+
+	public ForwardThread implements Runnalbe{
+		HashMap<Integer,ArrayList<Header> >	dataQueue;
+		private Socket socket;
+		ForwardThread(Socket socket,HashMap<Integer,ArrayList<Header> > dataQueue,HashMap<Integer,ArrayList<Header> > workQueue){
+			this.socket = socket;
+			this.dataQueue = dataQueue;
+		}
+		public void run(){
+			while(true){
+				
+			}
+		}
+	}
+
+	public RequestThread implements Runnalbe{
+		private Socket socket;
+		HashMap<Integer,ArrayList<Header> > workQueue;
+		RequestThread(Socket socket,HashMap<Integer,ArrayList<Header> > workQueue){
+			this.socket = socket;
+			this.workQueue = workQueue;
+		}
+		public void run(){
+			while(true){
+				ObjectInputStream inputstream = new ObjectInputStream(temp_socket.getInputStream());
+				try{
+					Header temp_h = (Header) inputstream.readObject();
+					ArrayList<Header> temp_list = workQueue.get(temp_h.room);
+					temp_list.add(temp_h);
+				}catch(ClassNotFoundException e){
+					System.out.println("in thread,class not found");
+				}
+			}
 		}
 	}
 }
